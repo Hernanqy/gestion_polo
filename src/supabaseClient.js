9275ff004bc810502e6c90179af9f495ba2cb312
Hernanqy <desarrolladoryotti@gmail.com>
2026-06-24 10:05:46 -0300

    fdf

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,16 +1,6 @@
 ﻿import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =
-  import.meta.env.VITE_SUPABASE_URL ||
-  "https://bvirrjlhcqnbzlpoveam.supabase.co";
-
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
-
-console.log("SUPABASE URL:", supabaseUrl);
-console.log("SUPABASE KEY EXISTE:", Boolean(supabaseKey));
-
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Faltan datos de Supabase en .env.local");
-}
+const supabaseUrl = "https://bvirrjlhcqnbzlpoveam.supabase.co";
+const supabaseKey = "sb_publishable_6B9CHvv8JuDaxwB3OGEBdQ_1u6eOoPa";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
